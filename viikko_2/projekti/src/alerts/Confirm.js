18a5285e828f19a9669f61aa536b0490ed2c66ef
tailsUp <114416460+tailsUp@users.noreply.github.com>
2023-08-10 16:51:52 +0300

    Delete viikko_2/projekti directory
    Poistetaan projektit githubista. Niille ei ole tarvetta.

--- viikko_2/projekti/src/alerts/Confirm.js
+++
@@ -1,8 +0,0 @@
-const ConfirmNumberChange = (duplicate, newNumber) => {
-    const ok = window.confirm(`${duplicate.name} is already in the phonebook, replace the old number with a new one?`)
-    if(ok) {
-      console.log(`User has decided to update ${duplicate.name} old number: ${duplicate.number} to new number: ${newNumber}`)
-    }
-}
-
-export default ConfirmNumberChange
