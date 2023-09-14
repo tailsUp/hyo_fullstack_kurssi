daae23a8e974a20ea151d4af3294129134cac635
tailsUp <sorsa.git@gmail.com>
2023-09-14 15:42:14 +0300

    Viikko 5 projekti sisään. Tehtävät tehty kohtaan 5.2 asti.

---
+++ viikko_5/blogs/backend/controllers/blogs.js
@@ -0,0 +1,98 @@
+const blogsRouter = require('express').Router()
+const middleware = require('../utils/middleware')
+const jwt = require('jsonwebtoken')
+const Blogs = require('../models/blogs')
+
+/**
+ * Funktio palauttaa tietokannan kaiken sisällön JSON olioina.
+ */
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blogs.find({})
+    console.log('GET ALL BLOG POSTS')
+    //const blogs = await Blogs.find({}).populate('user', { username: 1, name: 1 })
+    response.json(blogs)
+})
+
+/**
+ * Funktio palauttaa yksittäisen olion tietokannasta, jos sellaisen löytää.
+ */
+blogsRouter.get('/:id', async (request, response) => {
+    const blogs = await Blogs.findById(request.params.id)
+    if (blogs) {
+        response.json(blogs)
+    } else {
+        response.status(404).json(blogs)
+    }
+})
+
+/**
+ * Funktio lisää olion tietokantaan.
+ */
+blogsRouter.post('/', async (request, response, next) => {
+    const body = request.body
+    request = await middleware.userExtractor(request, response, next)
+    request = await middleware.tokenExtractor(request, response, next)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+
+    //const user = await Users.findById(body.userId)
+    if (request.user) {
+        const user = request.user
+
+        const blogs = new Blogs({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes,
+            user: user._id
+        })
+
+        const savedBlog = await blogs.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+        response.status(201).json(savedBlog)
+    }
+
+})
+
+/**
+ * Funktio poistaa tietokannasta olion, jos authorization token on ok. Jos authorization token puuttuu niin ilmoitetaan väärästä tokenista.
+ * (Viikko 4 tehtävä 4.21)
+ */
+blogsRouter.delete('/:id', async (request, response) => {
+    const deleteID = request.params.id
+    request = middleware.tokenExtractor(request, response)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid - NOTHING was deleted' })
+    }
+    else {
+        await Blogs.findByIdAndRemove(deleteID)
+        response.status(204).end()
+    }
+})
+
+/**
+ * Funtkio päivittää olemassaolevan olion tietokannassa.
+ */
+blogsRouter.put('/:id', (request, response, next) => {
+    const body = request.body
+
+    const blogs = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    }
+
+    Blogs.findByIdAndUpdate(request.params.id, blogs, { new: true })
+        .then(updatedBlogs => {
+            response.json(updatedBlogs)
+        })
+        .catch(error => next(error))
+})
+
+module.exports = blogsRouter
---
+++ viikko_5/blogs/backend/utils/middleware.js
@@ -0,0 +1,115 @@
+const logger = require('./logger')
+const Users = require('../models/users')
+
+/**
+ * Funktio lokittaa konsoliin pyynnön tietoja.
+ * @param {Request} request     - pyyntö.
+ * @param {Response} response   - vastaus.
+ * @param {Next} next           - eteenpäin siirrettävä virheviesti.
+ */
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:  ', request.path)
+  logger.info('Body:  ', request.body)
+  logger.info('---')
+  next()
+}
+
+/**
+ * Funktio lokittaa virheviestin jos kutsutaan osoitetta jota applikaatiossa ei ole.
+ * @param {Request} request     - pyyntö.
+ * @param {Response} response   - vastaus. 
+ */
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+/**
+ * Funktio lokittaa virheviestit kun tiedon käsittelyssä tapahtuu virhe.
+ * @param {Error} error       - tapahtunut virhe. 
+ * @param {Request} request   - pyyntö. 
+ * @param {Response} response - vastaus.
+ * @param {Next} next         - eteenpäin siirrettävä virheviesti.
+ * @returns 
+ */
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') 
+  {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  else if (error.name === 'ValidationError') 
+  {
+    return response.status(400).json({ error: error.message })
+  } 
+  else if (error.name === 'JsonWebTokenError') 
+  {
+    return response.status(400).json({ error: 'token missing or invalid' })
+  }
+  else if (error.name === 'TokenExpiredError') 
+  {    
+    return response.status(401).json({ error: 'token expired' })
+  }
+  next(error)
+}
+
+/**
+ * Funktio etsii headereista Bearer authorization tiedon, muokkaa sen ja asettaa token kenttään.
+ * @param {Request} request 
+ * @param {Response} response 
+ * @param {Next} next 
+ * @returns request
+ */
+const tokenExtractor = (request, response, next) => {
+  try 
+  {
+    request.rawHeaders.map(function(_h)
+    {
+      if(_h.includes("Bearer")) 
+      {
+        request.token = _h.replace('Bearer ', '')
+      }
+        return _h
+      });
+    return request
+  }
+  catch (error) 
+  {
+    console.log('Error in converting token from header to request. ', error)
+    next()
+  }
+}
+
+/**
+ * Funktio etsii id:n perusteella käyttäjän tietokannasta ja palauttaa sen requestin user kentässä.
+ * @param {Request} request 
+ * @param {Response} response 
+ * @param {Next} next 
+ * @returns request
+ */
+const userExtractor = async (request, response, next) => {
+  try 
+  {
+    const userID = request.body.userId || request.body.user
+    const user = await Users.findById(userID)
+    if(user) 
+    {
+      request.user = user
+    }
+    return request
+  }
+  catch (error)
+  {
+    console.log('Virhe käyttäjän haussa: ', error)
+    next()
+  }
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+  tokenExtractor,
+  userExtractor,
+}
