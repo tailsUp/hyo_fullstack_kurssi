bd48090fb64829e90bd776b4227d1e08225d9933
tailsUp <sorsa.git@gmail.com>
2023-10-06 11:42:39 +0300

    lisatty node env tyhjänä

--- viikko_5/blogs/backend/app.js
+++ viikko_5/blogs/backend/app.js
@@ -3,9 +3,6 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
-//Mongoose:
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
 //Utils:
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -14,6 +11,9 @@ const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+//Mongoose:
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
