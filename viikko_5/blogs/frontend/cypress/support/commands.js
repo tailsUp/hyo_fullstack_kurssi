b34b96a9534576d1fd797246f9308c673176f5ad
tailsUp <sorsa.git@gmail.com>
2023-10-12 10:35:15 +0300

    muutettu portti takaisin 3001

--- viikko_5/blogs/frontend/cypress/support/commands.js
+++ viikko_5/blogs/frontend/cypress/support/commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('login', ({ username, password }) => {
-  //cy.request('POST', 'http://localhost:3001/api/login', { username, password }).then(({ body }) => {
-  cy.request('POST', 'http://localhost:3003/api/login', { username, password }).then(({ body }) => {
+  cy.request('POST', 'http://localhost:3001/api/login', { username, password }).then(({ body }) => {
+  //cy.request('POST', 'http://localhost:3003/api/login', { username, password }).then(({ body }) => {
     localStorage.setItem('loggedNoteappUser', JSON.stringify(body))
     cy.visit('http://localhost:5173')
   })
@@ -8,8 +8,8 @@ Cypress.Commands.add('login', ({ username, password }) => {
 
 Cypress.Commands.add('createBlog', ({ title, author, url, likes, user }) => {
   cy.request({
-    //url: 'http://localhost:3001/api/blogs',
-    url: 'http://localhost:3003/api/blogs',
+    url: 'http://localhost:3001/api/blogs',
+    //url: 'http://localhost:3003/api/blogs',
     method: 'POST',
     body: { title, author, url, likes, user },
     headers: {
--- viikko_5/blogs/frontend/vite.config.js
+++ viikko_5/blogs/frontend/vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        //target: 'http://localhost:3001',
-        target: 'http://localhost:3003',
+        target: 'http://localhost:3001',
+        //target: 'http://localhost:3003',
         changeOrigin: true,
       },
     }
