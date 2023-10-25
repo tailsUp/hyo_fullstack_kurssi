591b92866eeb706ea1c2d9cb0203e56b6f9722f1
tailsUp <sorsa.git@gmail.com>
2023-10-25 15:59:50 +0300

    ASD3

--- viikko_5/blogs/backend/app.js
+++ viikko_5/blogs/backend/app.js
@@ -45,6 +45,7 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
+app.use(express.static('dist'))
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
@@ -54,6 +55,6 @@ app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
-app.use(express.static('dist'))
+
 
 module.exports = app
