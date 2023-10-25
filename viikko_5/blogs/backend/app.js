5bb19d6e1db6bfd7ddcdbb97429fcb593ff50510
tailsUp <sorsa.git@gmail.com>
2023-10-25 15:55:37 +0300

    ASD2

--- viikko_5/blogs/backend/app.js
+++ viikko_5/blogs/backend/app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-app.use(express.static('dist'))
+
 //Controller:
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
@@ -54,5 +54,6 @@ app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+app.use(express.static('dist'))
 
 module.exports = app
