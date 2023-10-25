2d7cd3b899388f57f5b4093555bc985b2795241c
tailsUp <sorsa.git@gmail.com>
2023-10-25 15:37:10 +0300

    lisätty dist komento backebdiin

--- viikko_5/blogs/backend/app.js
+++ viikko_5/blogs/backend/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+app.use(express.static('dist'))
 //Utils:
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
