0db2a6d92363ad574d57344ab45f5e038993cff7
tailsUp <sorsa.git@gmail.com>
2023-10-29 22:15:16 +0200

    vaihdettu index.js portti 3001 --> 3003

--- viikko_5/blogs/backend/index.js
+++ viikko_5/blogs/backend/index.js
@@ -5,6 +5,6 @@ const logger = require('./utils/logger')
 /**
  * Funktio kirjaa portin jossa ohjelma pyörii.
  */
-app.listen(3001, () => {
+app.listen(3003, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
