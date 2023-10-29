cfbbf2adf8a4b8b9490e1a8da70068cbcd8409af
tailsUp <sorsa.git@gmail.com>
2023-10-29 21:55:22 +0200

    dist ylin appeissa

--- viikko_5/blogs/backend/app.js
+++ viikko_5/blogs/backend/app.js
@@ -41,6 +41,7 @@ mongoose.connect(config.MONGODB_URI)
         logger.error('error connection to MongoDB:', error.message)
     })
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
@@ -54,6 +55,5 @@ app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
-app.use(express.static('dist'))
 
 module.exports = app
