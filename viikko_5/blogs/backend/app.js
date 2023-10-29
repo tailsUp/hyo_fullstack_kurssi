2c5aaa87b1a0e402db424eb214466b6256a8e740
tailsUp <sorsa.git@gmail.com>
2023-10-29 22:37:05 +0200

    dist siirretty appien ensimmäisen osan alle

--- viikko_5/blogs/backend/app.js
+++ viikko_5/blogs/backend/app.js
@@ -41,10 +41,10 @@ mongoose.connect(config.MONGODB_URI)
         logger.error('error connection to MongoDB:', error.message)
     })
 
-app.use(express.static('dist'))
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(express.static('dist'))
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
