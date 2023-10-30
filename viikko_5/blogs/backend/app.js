980f1a5506b3484e11a04816489e5c033a6ce0ce
tailsUp <sorsa.git@gmail.com>
2023-10-30 15:19:14 +0200

    dist siirretty 3

--- viikko_5/blogs/backend/app.js
+++ viikko_5/blogs/backend/app.js
@@ -41,13 +41,13 @@ mongoose.connect(config.MONGODB_URI)
         logger.error('error connection to MongoDB:', error.message)
     })
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 
 //onko distin ja buildin järjestyksellä väliä???
 
-app.use(express.static('dist'))
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
